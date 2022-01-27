e79678810a2c3ca62ea38c71188c7f726282b564
aviranjan07 <ad3447578@gmail.com>
2022-01-27 11:54:30 +0530

    added remobe and show functions

--- tools.js
+++ tools.js
@@ -1,4 +1,7 @@
+let toolsCont = document.querySelector(".tools-cont");
 let optionCont = document.querySelector(".options-cont");
+let pencilToolCont = document.querySelector(".pencil-tool-cont");
+let eraserToolCont = document.querySelector(".eraser-tool-cont");
 let optionsFlag = true;
 
 //true - tools show and false - hide tools
@@ -13,9 +16,14 @@ function openTools() {
     let iconElem = optionCont.children[0];
     iconElem.classList.remove("fa-times");
     iconElem.classList.add("fa-bars");
+    toolsCont.style.display = "flex";
 }
 function closeTools() {
     let iconElem = optionCont.children[0];
     iconElem.classList.remove("fa-bars");
     iconElem.classList.add("fa-times");
+    toolsCont.style.display = "none";
+    pencilToolCont.style.display = "none";
+    eraserToolCont.style.display = "none";    
+
 }
