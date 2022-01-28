d2945266321ffc781947412b77183c17b1c86686
aviranjan07 <ad3447578@gmail.com>
2022-01-28 10:26:48 +0530

    added linedraw functions

--- canves.js
+++ canves.js
@@ -2,13 +2,38 @@ let canves = document.querySelector("canvas");
 canves.width = window.innerWidth;
 canves.height = window.innerHeight;
 
+let mouseDown = false;
+
 //API
 let tool = canves.getContext("2d");
 
-tool.strokeStyle = "blue";
-tool.lineWidth = "8";
+tool.strokeStyle = "red";
+tool.lineWidth = "3";
 
-tool.beginPath();
-tool.moveTo(10, 10);
-tool.lineTo(100, 150);
-tool.stroke();
+//mousedown - start new path, mousemove- path fill
+canves.addEventListener("mousedown", (e) => {
+    mouseDown = true;
+    beginPath({
+        x: e.clientX,
+        y: e.clientY
+    })
+})
+canves.addEventListener("mousemove", (e) => {
+    if(mouseDown) drawStroke({
+        x: e.clientX,
+        y: e.clientY
+    })
+})
+canves.addEventListener("mouseup", (e) => {
+    mouseDown = false;
+})
+
+function beginPath(strokeObj) {
+    tool.beginPath();
+    tool.moveTo(strokeObj.x, strokeObj.y);
+}
+function drawStroke(strokeObj) {
+    tool.lineTo(strokeObj.x, strokeObj.y);
+    tool.stroke();
+}
+
